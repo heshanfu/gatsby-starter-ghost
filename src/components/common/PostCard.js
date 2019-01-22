c54b87e87fb9836e8f5b6f88c1d3f99408ed2a1f
Frank He <heshanfu@gmail.com>
2019-01-22 20:26:24 +0800

    Update PostCard.js

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -20,20 +20,6 @@ const PostCard = ({ post }) => {
                 <h2 className="post-card-title">{post.title}</h2>
             </header>
             <section className="post-card-excerpt">{post.excerpt}</section>
-            <footer className="post-card-footer">
-                <div className="post-card-footer-left">
-                    <div className="post-card-avatar">
-                        {post.primary_author.profile_image ?
-                            <img className="author-profile-image" src={post.primary_author.profile_image} alt={post.primary_author.name}/> :
-                            <img className="default-avatar" src="/images/icons/avatar.svg" alt={post.primary_author.name}/>
-                        }
-                    </div>
-                    <span>{ post.primary_author.name }</span>
-                </div>
-                <div className="post-card-footer-right">
-                    <div>{readingTime}</div>
-                </div>
-            </footer>
         </Link>
     )
 }
