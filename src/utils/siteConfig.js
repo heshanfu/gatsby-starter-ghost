39a4f6ec926809741715e5fcf8321d30f1b0eda6
Frank He <heshanfu@gmail.com>
2019-01-22 21:29:14 +0800

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-    siteUrl: `https://gatsby.ghost.org`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://www.sungz.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatbsy`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `阳光格子`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `阳光格子分享软件下载、精品网络资源等，以简洁明了的文字与图片介绍各种有趣、实用的网络相关的资讯与使用教学，让大家在各种简易的教学文章中，获得一些有用的技能提升。`, // This allows an alternative site description for meta data for pages.
 
     shareImage: `/images/Ghost-Docs.jpg`, // fallback image for meta data. Will be used, when no post/tag/author image specified. 1200x1200 is recommended
     shareImageWidth: 1000, // Change to the width of your default share image
