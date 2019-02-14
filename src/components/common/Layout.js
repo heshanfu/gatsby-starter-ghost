6177eb1d372f4752df0f7370080fbc8636936893
Frank He <heshanfu@gmail.com>
2019-02-14 12:02:51 +0800

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -35,40 +35,30 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
             <div className="viewport-top">
                 {/* The main header section on top of the screen */}
         
-  <header className="site-head">
-                    <div className="container">
-                        <div className="site-mast">
-                            <div className="site-mast-left">
-                                <Link to="/">
+ <header class="cd-auto-hide-header">
+	<div class="logo"> <Link to="/">
                                     {site.logo ?
                                         <img className="site-logo" src={site.logo} alt={site.title} />
                                         : <Img fixed={data.file.childImageSharp.fixed} alt={site.title} />
                                     }
-                                </Link>
-                            </div>
-                            <div className="site-mast-right">
-                                { site.twitter && <a href={ twitterUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /></a>}
-                                { site.facebook && <a href={ facebookUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /></a>}
-                                <a className="site-nav-item" href={ `https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/` } target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a>
-                            </div>
-                        </div>
-                        { isHome ?
-                            <div className="site-banner">
-                                <h1 className="site-banner-title">{site.title}</h1>
-                                <p className="site-banner-desc">{site.description}</p>
-                            </div> :
-                            null}
-                        <nav className="site-nav">
-                            <div className="site-nav-left">
-                                {/* The navigation items as setup in Ghost */}
-                                <Navigation data={site.navigation} navClass="site-nav-item" />
-                            </div>
-                            <div className="site-nav-right">
-                                <Link className="site-nav-button" to="/about">About</Link>
-                            </div>
-                        </nav>
-                    </div>
-                </header>
+                                </Link></div>
+
+	<nav class="cd-primary-nav">
+		<a href="#cd-navigation" class="nav-trigger">
+			<span>
+				<em aria-hidden="true"></em>
+				Menu
+			</span>
+		</a> <!-- .nav-trigger -->
+
+		<ul id="cd-navigation">
+		 {/* The navigation items as setup in Ghost */}
+                                
+			<li><Navigation data={site.navigation} navClass="site-nav-item" /></li>
+
+		</ul>
+	</nav> <!-- .cd-primary-nav -->
+</header> <!-- .cd-auto-hide-header -->
 
 
                 <main className="site-main">
